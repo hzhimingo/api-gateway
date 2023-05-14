a09420002f4e5f9f80b1da26079c931eab207399
huangzhiming <huangzhimingme@foxmail.com>
2023-05-14 13:05:38 +0800

    init project

---
+++ gateway-register-center/src/main/java/com/hzhimingo/App.java
@@ -0,0 +1,13 @@
+package com.hzhimingo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
